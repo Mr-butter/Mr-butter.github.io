410e010f3d0122e52590680f09b4c71d00628c4f
TsukinoHikari <89628687+TsukinoHikari@users.noreply.github.com>
2022-05-03 17:43:38 +0900

    test temp

---
+++ src/data/aboutData.js
@@ -0,0 +1,6 @@
+export const aboutData = {
+    title: "Who I am",
+    description1: "안녕하세요. 꿈꾸는 개발자 박성근 입니다.",
+    description2: "I am a student at Masai School, currently learning Fullstack Web-Development. A project that I worked on towards the end of my Master’s in History set me on this path of a web developer. The project involved looking at movies on historical themes that could be considered as an educational tool. The website was not the focus of the project, but working on it was such a thrilling experience that a career in this felt like a right fit. Other than this I am a bibliophile, who not just loves reading books but also buying and maintaining a personal library.",
+    image: 2
+}
